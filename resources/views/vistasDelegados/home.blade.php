f1f06537c86b4757f32baab89c12d90ef38d26e5
Pablete <pablo.zuniga.vale@gmail.com>
2018-11-13 11:30:10 -0300

    commit interfaz reserva

--- resources/views/vistasDelegados/home.blade.php
+++ resources/views/vistasDelegados/home.blade.php
@@ -14,10 +14,73 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <form class="bd-example">
+                        
+                        <fieldset>
+
+                          <p>
+                            <label for="select">Selecione un complejo deportivo</label>
+                            <select id="select">
+                                <option value="">Selecione</option>
+                                <option value="">Gimnacio CC </option>
+                                <option value="">Estadio Español</option>
+                            </select>
+                          </p>
+
+                          <p>
+                            <label for="select">Selecione una cancha</label>
+                            <select id="select">
+                                <option value="">Cancha N°1</option>
+                                <option value="">Cancha N°2</option>
+                                <option value="">Cancha N°3</option>
+                            </select>
+                          </p>
+                      
+                          <p>
+                            <label for="date">Selecione el dia</label>
+                            <input type="date" id="date" />
+                          </p>
+                      
+                          <p>
+                            <fieldset class="form-group">
+                                <div class="row">
+                                  <legend class="col-form-label col-sm-2 pt-0">Bloques de horario</legend>
+                                  <div class="col-sm-10">
+                                    <div class="form-check">
+                                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1" checked>
+                                      <label class="form-check-label" for="gridRadios1">
+                                        First radio
+                                      </label>
+                                    </div>
+                                    <div class="form-check">
+                                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="option2">
+                                      <label class="form-check-label" for="gridRadios2">
+                                        Second radio
+                                      </label>
+                                    </div>
+                                    <div class="form-check disabled">
+                                      <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="option3" disabled>
+                                      <label class="form-check-label" for="gridRadios3">
+                                        Third disabled radio
+                                      </label>
+                                    </div>
+                                  </div>
+                                </div>
+                              </fieldset>
+                          </p>
+                      
+                          <p>
+                            <button type="submit">Reservar</button>
+                        
+                          </p>
+                      
+                        </fieldset>
+                        
+                      </form>
                     
-                    Estas Logeado!
                 </div>
             </div>
+            
 
         </div>
     </div>
