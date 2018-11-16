aac9f0dd70b8dfb47dea1ab030622d5863b66484
Pablete <pablo.zuniga.vale@gmail.com>
2018-11-15 22:20:20 -0300

    middleware auth 2
    ñeñeññeñeñe

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,9 +36,9 @@
                        
                         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div class="navbar-nav">
-                                <a class="nav-item nav-link active" href="{{ route('infoAdmin') }}">Perfil Delegado <span class="sr-only">(current)</span></a>
-                                <a class="nav-item nav-link" href="#">Features</a>
-                                <a class="nav-item nav-link" href="#">Pricing</a>
+                                <a class="nav-item nav-link" href="{{ route('login') }}">Home</a>
+                                <a class="nav-item nav-link active" href="{{ route('infoAdmin') }}">Perfil Delegado</a>
+                                <a class="nav-item nav-link" href="#">Ingresar otra vista</a>
                             </div>
                         </div>
                     </nav>
