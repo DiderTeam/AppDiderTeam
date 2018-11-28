1f44bc62bae9f894e8ca44304cd7ef30c2fb95f4
Sebastian Sandoval <sebastianlsf96@gmail.com>
2018-11-28 13:54:25 -0300

    Update Cancha.php

--- app/Cancha.php
+++ app/Cancha.php
@@ -3,15 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\ComplejoDeportivo as ComplejoDeportivo;
 
 class Cancha extends Model
 {
     protected $table = 'canchas';
     protected $primaryKey = 'id';
-    protected $fillable = ['nombre','disponible'];
+    protected $fillable = ['nombre','disponible','idComplejoDeportivo'];
 
     public function ComplejoDeportivo(){
-        return $this->belongsTo('App\ComplejoDeportivo');
+        return $this->belongsTo('ComplejoDeportivo','idComplejoDeportivo');
     }
 
 }
+ 
