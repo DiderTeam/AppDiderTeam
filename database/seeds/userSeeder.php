53c880dd7b1ecae2e24185b3c7feee5a468a2381
Pablete <pablo.zuniga.vale@gmail.com>
2018-11-30 19:22:16 -0300

    Cambios

--- database/seeds/userSeeder.php
+++ database/seeds/userSeeder.php
@@ -12,20 +12,6 @@ class userSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Pablo Zuñiga',
-            'email' => 'pablo.zuniga.vale@gmail.com',
-            'password' => bcrypt('Laravel'),
-        ]);
-        DB::table('users')->insert([
-            'name' => 'Sebastian Sandoval ',
-            'email' => 'Seba_s@gmail.com',
-            'password' => bcrypt('Laravel2'),
-        ]);
-        DB::table('users')->insert([
-            'name' => 'Rodrigo Loyola',
-            'email' => 'R_Loyola@gmail.com',
-            'password' => bcrypt('Laravel3'),
-        ]);
+        //
     }
 }
